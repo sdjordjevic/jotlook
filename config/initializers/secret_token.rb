409d0a48e13e3504024cabefadf7d4b399349770
Srdjan Djordjevic <srdjan.djordjevic.89@gmail.com>
2011-10-21 20:43:20 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jotlook::Application.config.secret_token = '1bcaf0d3ef1c454fae9750d09096b86abac81281d14f3fa781497cb5e7892b735e4fae8c10e90be79b895654370b89b3b3d6b406b39c0c308d6fec79e053398d'
